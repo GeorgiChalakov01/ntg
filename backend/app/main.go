0a7b997eda64ad5448cd08ac1370d102ba528849
tazera <lyubomir.tsvetkov141@gmail.com>
2025-08-05 11:33:05 +0000

    page separations about us

--- backend/app/main.go
+++ backend/app/main.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-	"os"
 	"fmt"
 	"net/http"
-	"github.com/a-h/templ"
-	"github.com/GeorgiChalakov01/ntg/backend/app/pages/home"
+	"os"
+
+	aboutus "github.com/GeorgiChalakov01/ntg/backend/app/pages/about_us"
 	"github.com/GeorgiChalakov01/ntg/backend/app/pages/documents"
+	"github.com/GeorgiChalakov01/ntg/backend/app/pages/home"
+	"github.com/a-h/templ"
 )
 
 func main() {
 	http.Handle("/", http.RedirectHandler("/home", http.StatusSeeOther))
-	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
 		templ.Handler(home.Home()).ServeHTTP(w, r)
 	})
-	http.HandleFunc("/documents", func(w http.ResponseWriter, r *http.Request){
+	// Add route for about us page
+	http.HandleFunc("/about-us", func(w http.ResponseWriter, r *http.Request) {
+		aboutus.AboutUs().Render(r.Context(), w)
+	})
+	http.HandleFunc("/documents", func(w http.ResponseWriter, r *http.Request) {
 		templ.Handler(documents.Documents()).ServeHTTP(w, r)
 	})
-	
+
 	port := os.Getenv("BACKEND_PORT")
 	if port == "" {
 		port = "8080"
 	}
-	
+
 	fmt.Printf("Serving application on port %s ...\n", port)
-	http.ListenAndServe(":" + port, nil)
+	http.ListenAndServe(":"+port, nil)
 }
