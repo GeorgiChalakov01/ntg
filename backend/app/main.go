242bb1a992cc8d88f6aea2a2a890892a8ae22637
tazera <lyubomir.tsvetkov141@gmail.com>
2025-08-06 08:43:25 +0000

    small changes

--- backend/app/main.go
+++ backend/app/main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"os"
 
-	aboutus "github.com/GeorgiChalakov01/ntg/backend/app/pages/aboutus"
+	"github.com/GeorgiChalakov01/ntg/backend/app/pages/aboutus"
 	"github.com/GeorgiChalakov01/ntg/backend/app/pages/documents"
 	"github.com/GeorgiChalakov01/ntg/backend/app/pages/home"
 	"github.com/a-h/templ"
@@ -19,6 +19,10 @@ func main() {
 	// Add route for about us page
 	http.HandleFunc("/about-us", func(w http.ResponseWriter, r *http.Request) {
 		aboutus.AboutUs().Render(r.Context(), w)
+	})
+	// Add route for about us page
+	http.HandleFunc("/about-us2", func(w http.ResponseWriter, r *http.Request) {
+		aboutus.AboutUs2().Render(r.Context(), w)
 	})
 	http.HandleFunc("/documents", func(w http.ResponseWriter, r *http.Request) {
 		templ.Handler(documents.Documents()).ServeHTTP(w, r)
--- backend/app/pages/base/base_templ.go
+++ backend/app/pages/base/base_templ.go
@@ -41,7 +41,7 @@ func Base(title string, content templ.Component, styles templ.Component, scripts
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(title)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pages/base/base.templ`, Line: 14, Col: 18}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pages/base/base.templ`, Line: 15, Col: 16}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
